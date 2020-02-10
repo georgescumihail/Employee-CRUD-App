fa4b2bb0286ea76f1510cf6712267e76f7d30fb7
Mihail Georgescu <geo.mihail96@gmail.com>
2020-02-10 03:25:56 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div id="app-container">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+
+    return (
+        <div id="main-container">
+
+        </div>
+    );
+}
+
+export default MainPage;
